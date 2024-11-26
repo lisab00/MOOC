0d25f5ef716b444b2baf411d489428b6f68f09af
lisab00 <lisa.beer@tum.de>
2024-11-26 12:43:59 +0100

    task 3

--- exer1.py
+++ exer1.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import numpy as np
 import pandas as pd
+import random as rnd
 
 main_dir = Path(r'C:\Users\lihel\Documents\MOOC')
 
@@ -23,5 +24,15 @@ for i in range(len(df_data)):
 
 post_mean = m0 + Sigma0 @ A.T @ np.linalg.inv(Gamma + A @ Sigma0 @ A.T) @ (q- A @ m0)
 post_cov = Sigma0 - Sigma0 @ A.T @ np.linalg.inv(Gamma + A @ Sigma0 @ A.T) @ A @ Sigma0
-print(post_mean)
-print(post_cov)
+print(f"the posterior mean by formula is {post_mean}")
+print(f"the posterior coveriance by formula is {post_cov}")
+
+def prior(m, sigma):
+    return rnd.normal(m, sigma)
+
+def likelihood(q, x, A, Gamma):
+    temp = np.log(q) - A @ x
+    return np.exp(-1/2 * temp.T @ Gamma @ temp)
+
+def prod_prior_likeli(prior, likeli):
+    return prior * likeli
